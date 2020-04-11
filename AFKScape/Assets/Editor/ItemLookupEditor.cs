5e50877328a1d6929faa7b651828a7d6d5f9435d
Maurits Van Der Steur <mvds22@bath.ac.uk>
2020-04-11 18:36:26 +0400

    Added item lookup script in editor

---
+++ AFKScape/Assets/Editor/ItemLookupEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ItemLookup))]
+public class ItemLookupEditor : Editor
+{
+
+    string itemName;
+    long id = 0;
+    string status;
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Search Database for items", MessageType.Info);
+        ItemLookup itemLookup = (ItemLookup)target;
+        if (Database.items.Count == 0)
+        {
+            status = "Empty Database";
+        }
+        else
+        {
+            status = "Database Loaded";
+        }
+
+        EditorGUILayout.LabelField("Status: " + status, EditorStyles.boldLabel);
+
+        itemName = EditorGUILayout.TextField("Name:", itemName);
+        EditorGUILayout.LabelField("ID: " + id.ToString());
+
+        if (GUILayout.Button("Get ID"))
+        {
+            id = ItemLookup.GetItemId(itemName);
+        }
+    }
+}
---
+++ AFKScape/Assets/Scripts/ItemLookup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLookup : MonoBehaviour
+{
+    public static long GetItemId(string itemName)
+    {
+        foreach (KeyValuePair<long, Item> item in Database.items)
+        {
+            if (string.Equals(item.Value.name, itemName, System.StringComparison.OrdinalIgnoreCase))
+            {
+                return item.Key;
+            }
+        }
+        return -1;
+    }
+}
