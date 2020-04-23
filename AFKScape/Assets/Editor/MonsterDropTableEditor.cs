d8abcdb2195c77f2d37b55b356efec24a10ce096
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-04-24 01:33:00 +0400

    cleaned up

--- AFKScape/Assets/Editor/MonsterDropTableEditor.cs
+++ AFKScape/Assets/Editor/MonsterDropTableEditor.cs
@@ -14,10 +14,6 @@ public class MonsterDropTableEditor : Editor
 
     private string monsterName;
 
-    ReorderableList reorderableList;
-
-    bool foldout;
-
     int tableDepth;
 
     int index = 0;
@@ -27,17 +23,11 @@ public class MonsterDropTableEditor : Editor
         tableDepth = 0;
         monsterDropTableHandler = serializedObject.FindProperty("monsterDropTableHandler");
         generalDropTables = monsterDropTableHandler.FindPropertyRelative("generalDropTables");
-        //diceDropTable = serializedObject.FindProperty("monsterDropTable.diceDropTable");
-
-        //reorderableList = new ReorderableList(serializedObject, diceDropTable, true, true, true, true);
-
-        //reorderableList.drawHeaderCallback = DrawHeader;
     }
 
     public override void OnInspectorGUI()
     {
         MonsterDropTableAdder monsterDropTableAdder = (MonsterDropTableAdder)target;
-        //EditorGUILayout.PropertyField(monsterDropTableHandler);
         
         monsterName = EditorGUILayout.TextField("Name:", monsterName);
         if (GUILayout.Button("Load monster"))
@@ -46,7 +36,6 @@ public class MonsterDropTableEditor : Editor
         }
 
         EditorGUILayout.LabelField("General Info", EditorStyles.boldLabel);
-        //EditorGUILayout.PropertyField(monsterDropTableHandler.FindPropertyRelative("name"));
         EditorGUILayout.PropertyField(monsterDropTableHandler.FindPropertyRelative("rolls"));
 
         EditorGUILayout.Space(10);
@@ -101,112 +90,13 @@ public class MonsterDropTableEditor : Editor
         EditorGUI.indentLevel++;
         EditorGUILayout.PropertyField(monsterDropTableHandler.FindPropertyRelative("monsterDropTables"));
         EditorGUI.indentLevel--;
-        /*
-
-        EditorGUILayout.LabelField("100% Drop and Tertiary", EditorStyles.boldLabel);
-        //EditorGUILayout.PropertyField(monsterDropTable.FindPropertyRelative("rolls"));
-        EditorGUILayout.PropertyField(monsterDropTable.FindPropertyRelative("generalDropTable"));
-
-        EditorGUILayout.Space(10);
-
-        EditorGUILayout.LabelField("Uniques, Other and RDT", EditorStyles.boldLabel);
-        //EditorGUILayout.PropertyField(monsterDropTable.FindPropertyRelative("baseChance"));
-        //EditorGUILayout.PropertyField(monsterDropTable.FindPropertyRelative("indexMapping")); //TODO custom?
-
-        //reorderableList.DoLayoutList();
-
-        EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("Add Emtpy Table"))
-        {
-            //monsterDropTableAdder.monsterDropTable.diceDropTable.Add(new DiceDropTable());
-        }
-
-        if (GUILayout.Button("Add Json Table"))
-        {
-            tempDiceDropTable = JsonHandler.GetDiceDropTable();
-            //monsterDropTableAdder.monsterDropTable.diceDropTable.Add(tempDiceDropTable);
-        }
-        EditorGUILayout.EndHorizontal();
-
-        SerializedProperty tableDropTable = monsterDropTable.FindPropertyRelative("tableDropTable");
-        DrawTableDropTable(monsterDropTableAdder, tableDropTable);
-        //EditorGUILayout.PropertyField(tableDropTable, true);
-        /*
-        if (tableDropTable.isExpanded)
-        {
-            EditorGUI.indentLevel++;
-
-            foreach (SerializedProperty table in diceDropTable)
-            {
-                EditorGUILayout.PropertyField(table.FindPropertyRelative("name"));
-
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(table.FindPropertyRelative("lootList"));
-                EditorGUI.indentLevel--;
-            }
-
-            EditorGUI.indentLevel--;
-        }*/
-
+      
         EditorGUI.indentLevel--;
         serializedObject.ApplyModifiedProperties();
 
         if (GUILayout.Button("Apply"))
         {
             JsonHandler.SaveJsonFile(monsterDropTableAdder.monsterDropTableHandler, monsterName);
-
         }
-    }
-
-    void DrawHeader(Rect rect)
-    {
-        string name = "Re-order drop tables";
-        EditorGUI.LabelField(rect, name);
-    }
-
-    void DrawTableDropTable(MonsterDropTableAdder monsterDropTableAdder, SerializedProperty tableDropTable)
-    {
-        tableDepth++;        
-        EditorGUILayout.PropertyField(tableDropTable, false);
-
-        if (tableDropTable.isExpanded)
-        {
-            SerializedProperty isChild = tableDropTable.FindPropertyRelative("isChild");
-            EditorGUILayout.PropertyField(isChild);
-
-            if (!isChild.boolValue)
-            {
-                EditorGUILayout.PropertyField(tableDropTable.FindPropertyRelative("rolls"));
-            }
-
-            EditorGUILayout.PropertyField(tableDropTable.FindPropertyRelative("baseChance"));
-            EditorGUILayout.PropertyField(tableDropTable.FindPropertyRelative("indexMapping"));
-
-            index = EditorGUILayout.IntField("Index: ", index);
-            if (GUILayout.Button("Add Json Dice Table"))
-            {
-                //i dont even know, too deep to give up
-                if (tableDepth == 1)
-                {
-                   // monsterDropTableAdder.monsterDropTableHandler.tableDropTable.diceDropTables.Add(JsonHandler.GetDiceDropTable());
-                }
-                else if (tableDepth == 2)
-                {
-                    //monsterDropTableAdder.monsterDropTableHandler.tableDropTable.tableDropTables[index].diceDropTables.Add(JsonHandler.GetDiceDropTable());
-                }
-            }
-
-            EditorGUILayout.PropertyField(tableDropTable.FindPropertyRelative("diceDropTables"));
-
-            //SerializedProperty tableDropTables = tableDropTable.FindPropertyRelative("tableDropTables");
-            //EditorGUILayout.PropertyField(tableDropTables); //TODO if count > 0 display
-            if (tableDepth < 5)
-            {
-                SerializedProperty tableList = tableDropTable.FindPropertyRelative("tableDropTables");
-                DrawTableDropTable(monsterDropTableAdder, tableList.GetArrayElementAtIndex(0));
-            }
-        }
-
-        tableDepth--;
     }
 }
