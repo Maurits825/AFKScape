d23e0a3fe79e0b0428fcdbc29a3f3b9d8f5f7e46
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-01 17:27:03 +0400

    added skill icons, add radial progress bar

---
+++ AFKScape/Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public int minimum;
+    public int maximum;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Image icon;
+    public Color barColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+
+        fill.color = barColor;
+    }
+}
