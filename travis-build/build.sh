45141ec0dfd7fae802a6c406709d5d17d7f098b2
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-08 05:06:41 +0400

    ditch jobs/stages for now, added ut

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -2,17 +2,15 @@
 
 PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
 UNITY_BUILD_DIR=$(pwd)/Build
+UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 LOG_FILE=$UNITY_BUILD_DIR/unity-win.log
 
-
-ERROR_CODE=0
 echo "Items in project path ($PROJECT_PATH):"
 ls "$PROJECT_PATH"
 
-
 echo "Building project for Windows..."
 mkdir $UNITY_BUILD_DIR
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+${UNITY_PATH} \
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -21,17 +19,3 @@ mkdir $UNITY_BUILD_DIR
   -buildWindows64Player  "$(pwd)/build/win/ci-build.exe" \
   -quit \
   | tee "$LOG_FILE"
-  
-if [ $? = 0 ] ; then
-  echo "Building Windows exe completed successfully."
-  ERROR_CODE=0
-else
-  echo "Building Windows exe failed. Exited with $?."
-  ERROR_CODE=1
-fi
-
-#echo 'Build logs:'
-#cat $LOG_FILE
-
-echo "Finishing with code $ERROR_CODE"
-exit $ERROR_CODE
--- travis-build/run-unit-tests.sh
+++ travis-build/run-unit-tests.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
 
-echo "Skipping testing. No Tests implemented yet..."
-exit 0
+echo "[SYNG2] Running editor unit tests for ${UNITY_PROJECT_PATH}"
+
+${UNITY_PATH} \
+  -batchmode \
+  -silent-crashes \
+  -logFile "${TRAVIS_BUILD_DIR}/unity.unittests.log" \
+  -projectPath "${TRAVIS_BUILD_DIR}/${UNITY_PROJECT_PATH}/" \
+  -runEditorTests \
+  -editorTestsResultFile "${TRAVIS_BUILD_DIR}/unity.unittests.xml"
