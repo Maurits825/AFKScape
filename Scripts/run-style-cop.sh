b43cea6074903e5e6e9e3beb4caff92f7448a73a
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-19 18:17:34 +0400

    added style cop pkgs and script to run style cop

---
+++ Scripts/run-style-cop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+match='\(.*<Analyzer Include.*\)'
+insert='    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
+file='ScriptsAssembly.csproj'
+style_cop_csproj='ScriptsAssemblyStyleCop.csproj'
+
+echo "Creating StyleCop csproj"
+sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
+
+echo "Running Style Cop Analyzer"
+dotnet build $style_cop_csproj
+
+echo "Cleaning up"
+rm $style_cop_csproj
+echo "Done"
