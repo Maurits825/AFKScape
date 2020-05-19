f0a998e27569b05b630efb4080585981d9333dbb
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-19 18:32:16 +0400

    path fix

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -2,11 +2,9 @@
 
 match='\(.*<Analyzer Include.*\)'
 insert='    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
-file='ScriptsAssembly.csproj'
-style_cop_csproj='ScriptsAssemblyStyleCop.csproj'
+file='./AFKScape/ScriptsAssembly.csproj'
+style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 
-echo "--env --"
-ls
 echo "Creating StyleCop csproj"
 sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
 
