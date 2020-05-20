60db5a82e483a7c73ffa1a74999111378d98eb16
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-20 18:31:18 +0400

    fixed tests

--- AFKScape/Assets/TestPlayMode/SkillsTest.cs
+++ AFKScape/Assets/TestPlayMode/SkillsTest.cs
@@ -12,7 +12,7 @@ namespace Tests
         [Test]
         public void SkillsTestSimplePasses()
         {
-            Assert.IsTrue(false);
+            
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
