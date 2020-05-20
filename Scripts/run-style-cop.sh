510f9e427bbe563669477001cc8bc8903e8fda8a
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-21 03:59:06 +0400

    use image?

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading dependencies"
-sudo apt install mono-complete
+#sudo apt install mono-complete
 
 match='\(.*<Analyzer Include.*\)'
 insert='    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
