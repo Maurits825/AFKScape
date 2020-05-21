b44bbb28af4132cb03d6708fe9d326102fa8dd3b
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-21 18:38:35 +0400

    fix mult cmds, capital Package

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -4,7 +4,7 @@ echo "Check env"
 ls
 
 match='\(.*<Analyzer Include.*\)'
-insert='    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
+insert='    <Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
 file='./AFKScape/ScriptsAssembly.csproj'
 style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 
