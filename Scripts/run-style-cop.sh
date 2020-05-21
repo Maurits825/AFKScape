7a3cedf27166dea4608273853ff4311614d1f3c3
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-21 19:37:18 +0400

    use correct file path

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -12,6 +12,10 @@ style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 echo "Creating StyleCop csproj"
 sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
 
+file_path='C:\/Program Files\/Unity\/Hub\/Editor\/2019.3.7f1'
+linux_path='\/opt\/Unity'
+sed -i "s/$file_path/$linux_path/" $style_cop_csproj
+
 echo "Running Style Cop Analyzer"
 dotnet build $style_cop_csproj
 
