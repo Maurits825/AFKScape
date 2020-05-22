0ad4b1647efe96df9bb417a6063ac17895b0636f
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-22 04:46:10 +0400

    final hacks

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -20,11 +20,11 @@ file_path='C:\/Program Files\/Unity\/Hub\/Editor\/2019.3.7f1'
 linux_path='\/opt\/Unity'
 sed -i "s/$file_path/$linux_path/" $style_cop_csproj
 
-file_path='A:/repo/AFKScape/AFKScape/Library/ScriptAssemblies/UnityEditor.UI.dll'
-linux_path='/opt/Unity/Editor/Data/Resources/PackageManager/ProjectTemplates/libcache/com.unity.template.universal-7.1.8/ScriptAssemblies/UnityEditor.UI.dll'
+file_path='A:\/repo\/AFKScape\/AFKScape\/Library\/ScriptAssemblies\/UnityEditor.UI.dll'
+inux_path='\/opt\/Unity\/Editor\/Data\/Resources\/PackageManager\/ProjectTemplates\/libcache\/com.unity.template.universal-7.1.8\/ScriptAssemblies\/UnityEditor.UI.dll'
 sed -i "s/$file_path/$linux_path/" $style_cop_csproj
-file_path='A:/repo/AFKScape/AFKScape/Library/ScriptAssemblies/UnityEngine.UI.dll'
-linux_path='/opt/Unity/Editor/Data/Resources/PackageManager/ProjectTemplates/libcache/com.unity.template.universal-7.1.8/ScriptAssemblies/UnityEngine.UI.dll'
+file_path='A:\/repo\/AFKScape\/AFKScape\/Library\/ScriptAssemblies\/UnityEngine.UI.dll'
+linux_path='\/opt\/Unity\/Editor\/Data\/Resources\/PackageManager\/ProjectTemplates\/libcache\/com.unity.template.universal-7.1.8\/ScriptAssemblies\/UnityEngine.UI.dll'
 sed -i "s/$file_path/$linux_path/" $style_cop_csproj
 
 #remove unused dlls
@@ -32,6 +32,8 @@ dll_name='UnityEditor.WindowsStandalone.Extensions'
 sed -i "1N;$!N;s/.*$dll_name.*\n.*\n.*//;P;D" $style_cop_csproj
 dll_name='SyntaxTree.VisualStudio.Unity.Bridge'
 sed -i "1N;$!N;s/.*$dll_name.*\n.*\n.*//;P;D" $style_cop_csproj
+dll_name='Microsoft.Unity.Analyzers.dll'
+sed -i "s/.*$dll_name.*//" $style_cop_csproj
 
 repo_path='A:\/repo\/AFKScape\/'
 sed -i "s/$repo_path//" $style_cop_csproj
