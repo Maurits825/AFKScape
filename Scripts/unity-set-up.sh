26e16679c17a000cd1ed9b488c75a69d17456248
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-23 02:18:48 +0400

    random test

--- Scripts/unity-set-up.sh
+++ Scripts/unity-set-up.sh
@@ -4,8 +4,13 @@ echo "--- Activate License ---"
 /opt/Unity/Editor/Unity -batchmode -manualLicenseFile license.ulf -logfile /dev/stdout -nographics -quit
 
 echo "--- Sync solution ---"
-/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics -quit
+/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics -noUpm
 
-echo "--- Checking Env ---"
+echo "--- Checking AFKScape ---"
 ls AFKScape
+echo "--- Checking ls ../ ---"
+ls ../
+echo "--- Checking ls ../.. ---"
+ls ../..
+echo "--- Checking find ---"
 find ../ -name *.csproj
