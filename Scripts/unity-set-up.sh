d4a2768776277565d644f688e95658ecde5474a6
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-23 02:49:25 +0400

    randoms tuff

--- Scripts/unity-set-up.sh
+++ Scripts/unity-set-up.sh
@@ -3,8 +3,12 @@
 echo "--- Activate License ---"
 /opt/Unity/Editor/Unity -batchmode -manualLicenseFile license.ulf -logfile /dev/stdout -nographics -quit
 
+echo "--- open normal ---"
+/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -nographics -quit
 echo "--- Sync solution ---"
-/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics -noUpm
+/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics
+echo "--- again? ---"
+/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics
 
 echo "--- Checking AFKScape ---"
 ls AFKScape
