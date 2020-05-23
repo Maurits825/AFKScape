9c9b47c86516772fc8f1cd8b0cafbacf452eeb77
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-23 15:13:58 +0400

    final fixes

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -1,43 +1,17 @@
 #!/bin/bash
 
-match='\(.*<Analyzer Include.*\)'
-insert='    <Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n    <Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/>'
+match='\(<Project ToolsVersion.*\)'
+insert='<ItemGroup><Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.CodeFixes.dll" \/>\n<Analyzer Include="Packages\\StyleCop.Analyzers.1.1.118\\analyzers\\dotnet\\cs\\StyleCop.Analyzers.dll" \/><\/ItemGroup>'
 file='./AFKScape/ScriptsAssembly.csproj'
 style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 
-echo "Creating StyleCop csproj"
+echo "--- Creating StyleCop .csproj ---"
 sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
 
-#fix slashes
-sed -i 's/\\/\//g' $style_cop_csproj
-
-#set paths
-file_path='C:\/Program Files\/Unity\/Hub\/Editor\/2019.3.7f1'
-linux_path='\/opt\/Unity'
-sed -i "s/$file_path/$linux_path/" $style_cop_csproj
-
-file_path='A:\/repo\/AFKScape\/AFKScape\/Library\/ScriptAssemblies\/UnityEditor.UI.dll'
-inux_path='\/opt\/Unity\/Editor\/Data\/Resources\/PackageManager\/ProjectTemplates\/libcache\/com.unity.template.universal-7.1.8\/ScriptAssemblies\/UnityEditor.UI.dll'
-sed -i "s/$file_path/$linux_path/" $style_cop_csproj
-file_path='A:\/repo\/AFKScape\/AFKScape\/Library\/ScriptAssemblies\/UnityEngine.UI.dll'
-linux_path='\/opt\/Unity\/Editor\/Data\/Resources\/PackageManager\/ProjectTemplates\/libcache\/com.unity.template.universal-7.1.8\/ScriptAssemblies\/UnityEngine.UI.dll'
-sed -i "s/$file_path/$linux_path/" $style_cop_csproj
-
-#remove unused dlls
-dll_name='UnityEditor.WindowsStandalone.Extensions'
-sed -i "1N;$!N;s/.*$dll_name.*\n.*\n.*//;P;D" $style_cop_csproj
-dll_name='SyntaxTree.VisualStudio.Unity.Bridge'
-sed -i "1N;$!N;s/.*$dll_name.*\n.*\n.*//;P;D" $style_cop_csproj
-dll_name='Microsoft.Unity.Analyzers.dll'
-sed -i "s/.*$dll_name.*//" $style_cop_csproj
-
-repo_path='A:\/repo\/AFKScape\/'
-sed -i "s/$repo_path//" $style_cop_csproj
-
-echo "Running Style Cop Analyzer"
+echo "--- Running Style Cop Analyzer ---"
 export FrameworkPathOverride=/Library/Frameworks/Mono.framework/Versions/Current
 dotnet build $style_cop_csproj
 
-echo "Cleaning up"
+echo "--- Cleaning up ---"
 rm $style_cop_csproj
-echo "Done"
+echo "--- Done ---"
--- Scripts/unity-set-up.sh
+++ Scripts/unity-set-up.sh
@@ -3,12 +3,8 @@
 echo "--- Activate License ---"
 /opt/Unity/Editor/Unity -batchmode -manualLicenseFile license.ulf -logfile /dev/stdout -nographics -quit
 
-#echo "--- open normal ---"
-#/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -nographics -quit
 echo "--- Sync solution ---"
 /opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics
-#echo "--- again? ---"
-#/opt/Unity/Editor/Unity -batchmode -logFile /dev/stdout -projectPath AFKScape/ -executeMethod MainControllerEditor.createSLN -nographics
 
 echo "--- Checking AFKScape ---"
 ls AFKScape
