cff75f55a5d86419ff9f9a88a13280cf05f9b24c
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-24 22:57:09 +0400

    debug stuff

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -8,9 +8,12 @@ style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 echo "--- Creating StyleCop .csproj ---"
 sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
 
+echo "--- Displaying csproj ---"
+cat $style_cop_csproj
+
 echo "--- Running Style Cop Analyzer ---"
 export FrameworkPathOverride=/Library/Frameworks/Mono.framework/Versions/Current
-dotnet build $style_cop_csproj
+dotnet build $style_cop_csproj --verbosity diag
 
 echo "--- Cleaning up ---"
 rm $style_cop_csproj
