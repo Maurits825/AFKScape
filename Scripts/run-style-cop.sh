fabb41d19bd60c987dd7f3235fbb612683e98338
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-05-26 15:10:54 +0400

    style-cop fails if errors or warnings

--- Scripts/run-style-cop.sh
+++ Scripts/run-style-cop.sh
@@ -11,13 +11,26 @@ style_cop_csproj='./AFKScape/ScriptsAssemblyStyleCop.csproj'
 echo "--- Creating StyleCop .csproj ---"
 sed "s/$match/\1\n$insert/" $file > $style_cop_csproj
 
-echo "--- Displaying csproj ---"
-cat $style_cop_csproj
-
 echo "--- Running Style Cop Analyzer ---"
 export FrameworkPathOverride=/Library/Frameworks/Mono.framework/Versions/Current
-dotnet build $style_cop_csproj --verbosity n
+log_file='log.txt'
+dotnet build $style_cop_csproj --verbosity m > $log_file
+
+echo "--- Showing Results ---"
+cat $log_file
+
+warnings=$(sed -n -e 's/ *\(.*\) Warning(s)/\1/p' $log_file)
+errors=$(sed -n -e 's/ *\(.*\) Error(s)/\1/p' $log_file)
+
+if [[ $warnings -eq 0 && $errors -eq 0 ]]; then
+    exit_status='0'
+else
+    exit_status='1'
+fi
 
 echo "--- Cleaning up ---"
 rm $style_cop_csproj
+rm $log_file
 echo "--- Done ---"
+
+exit $exit_status
