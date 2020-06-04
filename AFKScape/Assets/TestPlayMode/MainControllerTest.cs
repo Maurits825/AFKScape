c50ddb94c1487aaa37c55dd5a3f181b0e0264b58
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-06-04 17:18:41 +0400

    added simple play mode test to load scene

---
+++ AFKScape/Assets/TestPlayMode/MainControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MainControllerTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MainControllerTestWithEnumeratorPasses()
+        {
+            SceneManager.LoadScene("MainScene");
+            yield return null;
+            yield return null;
+        }
+    }
+}
