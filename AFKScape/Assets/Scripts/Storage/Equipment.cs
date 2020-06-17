19b46994074ddbe97ddad81d2a98a2a43d8a073c
Maurits Van Der Steur <mauritsvdsteur@live.com>
2020-06-17 18:40:06 +0400

    handle 2h weapons

--- AFKScape/Assets/Scripts/Storage/Equipment.cs
+++ AFKScape/Assets/Scripts/Storage/Equipment.cs
@@ -24,35 +24,93 @@ public class Equipment
     }
 
     private long[] equipedItems = new long[11];
+    private bool isTwoHandedEquipped = false;
 
     public void Initialize(Inventory inventory)
     {
         this.inventory = inventory;
     }
 
-    //TODO ammo slot count and 2h
+    //TODO ammo slot count
+    //TODO button to show item stats UI and keep track of item stats/dmg bonuses
+    //TODO equip/unequip same item/slot, slot stays same, make new inv.add func? -- issue
     public void EquipItem(long id, EquipmentSlot slot)
     {
-        if (Database.items[id].equipable && CheckRequirements())
+        if (Database.items[id].equipableByPlayer && CheckRequirements())
         {
-            int slotIndex = (int)slot;
+            int slotIndex;
+            EquipmentSlot actualSlot;
+            if (slot == EquipmentSlot.twoHanded)
+            {
+                slotIndex = (int)EquipmentSlot.weapon;
+                actualSlot = EquipmentSlot.weapon;
+            }
+            else
+            {
+                slotIndex = (int)slot;
+                actualSlot = slot;
+            }
 
-            BigInteger amountRemoved = inventory.RemoveItem(id, 1);
+            BigInteger amountRemoved = inventory.RemoveItem(id, 1);            
 
-            if (equipedItems[slotIndex] != 0)
+            if (slot == EquipmentSlot.twoHanded)
+            {
+                if (equipedItems[(int)EquipmentSlot.weapon] != 0)
+                {
+                    UnEquipItem(equipedItems[(int)EquipmentSlot.weapon], EquipmentSlot.weapon);
+                }
+
+                if (equipedItems[(int)EquipmentSlot.shield] != 0)
+                {
+                    UnEquipItem(equipedItems[(int)EquipmentSlot.shield], EquipmentSlot.shield);
+                }
+
+                isTwoHandedEquipped = true;
+            }
+            else if (slot == EquipmentSlot.shield)
+            {
+                if (equipedItems[(int)EquipmentSlot.shield] != 0)
+                {
+                    UnEquipItem(equipedItems[(int)EquipmentSlot.shield], EquipmentSlot.shield);
+                }
+                else if (isTwoHandedEquipped)
+                {
+                    UnEquipItem(equipedItems[(int)EquipmentSlot.weapon], EquipmentSlot.weapon);
+                }
+
+                isTwoHandedEquipped = false;
+            }
+            else if (slot == EquipmentSlot.weapon)
+            {
+                if (equipedItems[(int)EquipmentSlot.weapon] != 0)
+                {
+                    UnEquipItem(equipedItems[(int)EquipmentSlot.weapon], EquipmentSlot.weapon);
+                }
+
+                isTwoHandedEquipped = false;
+            }
+            else if (equipedItems[slotIndex] != 0)
             {
                 UnEquipItem(equipedItems[slotIndex], slot);
             }
 
             equipedItems[slotIndex] = id;
-
-            EventManager.Instance.ItemEquipped(id, slot);
+            EventManager.Instance.ItemEquipped(id, actualSlot);
         }
     }
 
     public void UnEquipItem(long id, EquipmentSlot slot)
     {
-        int slotIndex = (int)slot;
+        int slotIndex;
+        if (slot == EquipmentSlot.twoHanded)
+        {
+            slotIndex = (int)EquipmentSlot.weapon;
+        }
+        else
+        {
+            slotIndex = (int)slot;
+        }
+
         equipedItems[slotIndex] = 0;
         inventory.AddItem(id, 1);
 
