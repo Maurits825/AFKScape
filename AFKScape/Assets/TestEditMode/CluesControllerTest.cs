6adc91389b01ec9fa750aaa63e4397edfa3e91fd
Maurits van der Steur <mauritsvdsteur@live.com>
2020-08-27 23:35:10 +0200

    added template for clues controller ut

---
+++ AFKScape/Assets/TestEditMode/CluesControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CluesControllerTest
+    {
+        private CluesController cluesController;
+        private Inventory inventory;
+        private Bank bank;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            Database.LoadBosses();
+            if (Database.items.Count != 0)
+            {
+                Database.items.Clear();
+            }
+            Database.LoadItems();
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            Database.bossesNames.Clear();
+            Database.items.Clear();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            EventManager.SetIntance(new EventManager());
+            cluesController = new CluesController();
+            inventory = new Inventory();
+            bank = new Bank();
+
+            cluesController.Initialize(inventory, bank);
+        }
+
+        [Test]
+        public void ClueGameLoopTest()
+        {
+            Monster monster = cluesController.cluesClasses[Database.cluesNames[0]];
+
+            int days = 1;
+            int seconds = days * 86400;
+            cluesController.OnClueSelected(Database.cluesNames[0]);
+            cluesController.ClueGameLoop(monster, seconds / 10);
+        }
+    }
+}
